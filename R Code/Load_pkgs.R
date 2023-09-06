2039e2eaa38af319c1b88e18322a46f11ad14b88
Victoria <126200017+victoriatchu@users.noreply.github.com>
2023-09-06 03:53:54 -0700

    Add files via upload

---
+++ R Code/Load_pkgs.R
@@ -0,0 +1,9 @@
+
+install.packages("tidyverse")
+install.packages("vegan")
+install.packages("epiDisplay")
+install.packages("ggpubr") 
+install.packages("rstatix")
+install.packages("patchwork")
+install.packages("viridis")
+install.packages("DESeq2")
